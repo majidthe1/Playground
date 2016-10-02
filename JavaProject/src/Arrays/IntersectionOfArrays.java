c1605555e480ec6c44d7532b274d456205c3235a
Syed, Abdul Majid <abdulmajid.syed@cengage.com>
2016-10-02 07:30:00 -0500

    Project Setup

---
+++ JavaProject/src/Arrays/IntersectionOfArrays.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+/**
+ * Created by asyed on 10/2/16.
+ * Program to find the intersection of 2 arrays
+
+ */
+public class IntersectionOfArrays {
+    public static void main(String[] args){
+        int[] a1 ={23, 1,4,56,3,0,12};
+        int[] a2 ={1,3,0,4,87,66,33};
+        System.out.println(a2[4]);
+    }
+
+    public static int[] intersection(int[] a1, int[] a2){
+        
+    }
+}
